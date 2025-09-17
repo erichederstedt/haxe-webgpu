9f79a5465708608e3c59d9f8f2cfd055449db35b
eric <eric.jonsson-hederstedt@hacksawstudios.com>
2025-09-17 18:04:28 +0200

    Fixed HTMLCanvasElement error

--- src/webgpu/HTMLCanvasElement.hx
+++ src/webgpu/HTMLCanvasElement.hx
@@ -1,5 +1,5 @@
 package webgpu;
 
 extern interface HTMLCanvasElement {
-	function getContext(contextId:haxe.extern.EitherType<htmlcanvaselement.ContextId,>):GPUCanvasContext;
-	}
+	function getContext(contextId:haxe.extern.EitherType<htmlcanvaselement.ContextId, String>):GPUCanvasContext;
+}
